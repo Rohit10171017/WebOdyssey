9dc8b12ce4804cf99a9b87933b97f9fb568ca3b0
Rohit10 <rsjabd17@gmail.com>
2025-12-17 19:28:29 +0530

    ADD : add logic to calculator

--- Projects/Calculator/calculator.js
+++ Projects/Calculator/calculator.js
@@ -1,4 +1,30 @@
-const input=document.getElementById("inputbox");
+const input = document.getElementById("inputbox");
 const button = document.querySelectorAll('button')
-console.log(input)
-console.log(button);
+
+let str = "";
+button.forEach((ele) =>
+{
+    ele.addEventListener('click',(e) => {
+        let innerHTML = e.target.innerHTML;
+
+        if(innerHTML === "AC")
+        {
+            str = "";
+            input.value = str;
+        }  
+        else if(innerHTML === "DEL" )
+        {
+            str = str.slice(0,str.length-1);
+            input.value= str;
+        }
+        else if(innerHTML === "=")
+        {
+            str = eval(str);
+            input.value = str;
+        }
+        else{
+            str += e.target.innerHTML;
+            input.value = str;
+        }
+    })
+})
